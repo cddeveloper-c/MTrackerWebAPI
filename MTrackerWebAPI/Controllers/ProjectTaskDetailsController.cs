419d0366406cde75c8abdc9f0673275a867b2e4b
SarnadeepMondal <74441574+SarnadeepMondal@users.noreply.github.com>
2023-04-08 10:34:01 +0530

    linq query implement

--- MTrackerWebAPI/Controllers/ProjectTaskDetailsController.cs
+++ MTrackerWebAPI/Controllers/ProjectTaskDetailsController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MTrackerWebAPI.Model;
 using MTrackerWebAPI.Model.ViewModel;
+using System.Linq;
 
 namespace MTrackerWebAPI.Controllers
 {
@@ -15,11 +17,23 @@ namespace MTrackerWebAPI.Controllers
         {
             _context = Dbcontext;
         }
-        //[HttpGet]
-        //public async Task<ActionResult<ProjectTaskDetailsViewModel>> GetProjectTaskDetails(int id)
-        //{
-        //   var projecttask = await _context.ProjectTasks.FindAsync(id);
+        [HttpGet]
+        public async Task<ActionResult<ProjectTaskDetailsViewModel>> GetProjectTaskDetails(int id)
+        {
+            //var projecttask = await _context.ProjectTasks.FindAsync(id);
+            var projecttask = await (from p in _context.ProjectTasks
+                               join r in _context.Resource on p.AssignedTo equals r.ResourceID
+                               join u in _context.UserStories on p.UserStoryID equals u.UserStoryID
+                               join pr in _context.Projects on u.ProjectID equals pr.ProjectID
+                               select new ProjectTaskDetailsViewModel
+                               {
+                                   ProjectTaskID = p.ProjectTaskID,
+                                   ResourceName = r.ResourceName,
+                                   ProjectName = pr.ProjectName,
+                                   Story = u.Story,
+                               }).ToListAsync();
 
-        //}
+            return Ok(projecttask);
+        }
     }
 }
